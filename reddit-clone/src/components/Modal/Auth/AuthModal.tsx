e58839c0f352bd517946d2ee2b14b9c946474691
Tina-Lightning <c.atherholt@gmail.com>
2023-04-03 07:46:07 -0400

    set up Login form

--- reddit-clone/src/components/Modal/Auth/AuthModal.tsx
+++ reddit-clone/src/components/Modal/Auth/AuthModal.tsx
@@ -13,7 +13,7 @@ import {
 import React from "react";
 import { useRecoilState } from "recoil";
 import { authModalState } from "../../../atoms/authModalAtom";
-//
+import AuthInputs from "./AuthInputs";
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
@@ -30,7 +30,7 @@ const AuthModal: React.FC = () => {
       <Modal isOpen={modalState.open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>
+          <ModalHeader textAlign="center">
             {modalState.view === "login" && "Login"}
             {modalState.view === "signup" && "Sign Up"}
             {modalState.view === "resetPassword" && "Reset Password"}
@@ -41,16 +41,16 @@ const AuthModal: React.FC = () => {
             flexDirection="column"
             alignItems="center"
             justifyContent="center"
+            pb={6}
           >
             <Flex
               direction="column"
               align="center"
               justify="center"
               width="70%"
-              border="1px solid red"
             >
-              {/* <OAuthButtons />
-                <AuthInputs /> */}
+              {/* <OAuthButtons /> */}
+              <AuthInputs />
               {/* <ResetPassword />  */}
             </Flex>
           </ModalBody>
