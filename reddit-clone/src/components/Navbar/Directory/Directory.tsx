447451cc9ead45368be81e3a2754429a42bba2ea
Tina-Lightning <c.atherholt@gmail.com>
2023-04-18 23:20:37 -0400

    continued working on dropdowns from the navbar

---
+++ reddit-clone/src/components/Navbar/Directory/Directory.tsx
@@ -0,0 +1,29 @@
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Flex, Icon, Menu, MenuButton, MenuList, Text } from "@chakra-ui/react";
+import React from "react";
+import { TiHome } from "react-icons/ti";
+
+const UserMenu: React.FC = () => {
+  return (
+    <Menu>
+      <MenuButton
+        cursor="pointer"
+        padding="0px 6px"
+        borderRadius="4px"
+        _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+      >
+        <Flex alignItems="center">
+          <Flex alignItems="center">
+            <Icon fontSize={24} mr={{ base: 1, md: 2 }} as={TiHome} />
+            <Flex display={{ base: "none", lg: "flex" }}>
+              <Text fontWeight={600}>Home</Text>
+            </Flex>
+          </Flex>
+          <ChevronDownIcon color="gray.500" />
+        </Flex>
+      </MenuButton>
+      <MenuList></MenuList>
+    </Menu>
+  );
+};
+export default UserMenu;
