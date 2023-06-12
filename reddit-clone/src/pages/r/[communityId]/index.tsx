a1c26e721c2e59a73ed95563c14cd587faeebd81
Tina-Lightning <c.atherholt@gmail.com>
2023-06-12 11:22:51 -0400

    started working on community pages

--- reddit-clone/src/pages/r/[communityId]/index.tsx
+++ reddit-clone/src/pages/r/[communityId]/index.tsx
@@ -1,9 +1,11 @@
+import NotFound from "@/components/Community/NotFound";
 import { Community } from "../../../atoms/communitiesAtom";
 import { firestore } from "../../../firebase/clientApp";
 import { doc, getDoc } from "firebase/firestore";
 import { GetServerSidePropsContext } from "next";
 import React from "react";
 import safeJsonStringify from "safe-json-stringify";
+import Header from "@/components/Community/Header";
 
 type CommunityPageProps = {
   communityData: Community;
@@ -11,7 +13,16 @@ type CommunityPageProps = {
 
 const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
   console.log("here is data", communityData);
-  return <div>Welcome to {communityData.id}</div>;
+
+  if (!communityData) {
+    return <NotFound />;
+  }
+
+  return (
+    <>
+      <Header communityData={communityData} />
+    </>
+  );
 };
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
@@ -26,9 +37,11 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
     return {
       props: {
-        communityData: JSON.parse(
-          safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
-        ),
+        communityData: communityDoc.exists()
+          ? JSON.parse(
+              safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+            )
+          : "",
       },
     };
   } catch (error) {
